2f3835c499e1a1a2ab6344eb723d4d1e4e5f8565
yosefus <yosalsoni@gmail.com>
2021-12-13 21:36:25 +0200

    new

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './footer.module.scss';
+import styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
